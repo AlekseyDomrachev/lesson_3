d9da57ce1b13b4b5af6fc4812c08a2a8c67655e7
AlekseyDomrachev <107096596+AlekseyDomrachev@users.noreply.github.com>
2022-08-10 15:55:56 +0300

    task ready

--- 01_days_in_month.py
+++ 01_days_in_month.py
@@ -10,5 +10,30 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
+if month == 1:
+        print(31, 'days')
+elif month == 2:
+       print(28, 'days')
+elif month == 3:
+        print(31, 'days')
+elif month == 4:
+        print(30, 'days')
+elif month == 5:
+        print(31, 'days')
+elif month == 6:
+        print(30, 'days')
+elif month == 7:
+       print(31, 'days')
+elif month == 8:
+        print(31, 'days')
+elif month == 9:
+        print(30, 'days')
+elif month == 10:
+        print(31, 'days')
+elif month == 11:
+        print(30, 'days')
+elif month == 12:
+        print(31, 'days')
+else:
+        print('Not correct input. Please use 1-12!')
 
-# TODO здесь ваш код
